763505969d4bf98f4be0ff72980b4030d664985d
Nicholas Wang <nickwang55@gmail.com>
2016-11-02 11:54:05 -0700

    Finish size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,22 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if ( size ) {
+      size--;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
