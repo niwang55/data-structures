b3e96dd4c4d65ea25e04f7cdbb705d1e6f19202e
Nicholas Wang <nickwang55@gmail.com>
2016-11-02 12:09:28 -0700

    Finish enqueue and dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,12 +8,26 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
     size++;
   };
 
   someInstance.dequeue = function() {
     if ( size ) {
+      var removedValue = storage[0];
+      delete storage[0];
       size--;
+
+      for( var key in storage ) {
+        var currVal = storage[key];
+
+        //Decrease each key in the storage by 1
+        storage[key-1] = currVal;
+
+        //Remove the value with the +1 key
+        delete storage[key];
+      }
+      return removedValue;
     }
   };
 
