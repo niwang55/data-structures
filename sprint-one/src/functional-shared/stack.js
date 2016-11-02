9c9264c4fc5b536ab8cb47f06f0e42b941c77b0d
Nicholas Wang <nickwang55@gmail.com>
2016-11-02 14:35:39 -0700

    Some work on push and pop

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.stackSize = 0;
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+		this.stackSize++;
+	},
+
+	pop: function() {
+		if( this.stackSize ) {
+			this.stackSize--;
+		}
+	},
+
+	size: function() {
+		return this.stackSize;
+	}
+};
 
 
