4dec3b8a8c3720f1d22abf77df5fc4c99894d34d
niwang55 <nickwang55@gmail.com>
2016-11-02 19:40:39 -0700

    Finish linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      var newNode = new Node(value);
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var nextNode = new Node(value);
+
+      list.tail.next = nextNode;
+      list.tail = list.tail.next;
+      
+    }
+
+
   };
 
   list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+
+    do {
+      if (currentNode.value === target) {
+        return true;
+      } 
+      currentNode = currentNode.next;
+    } while (currentNode !== null && currentNode.value !== undefined);
+
+    return false;
   };
 
   return list;
