929373ecb8e74f1a686ad3493c67ea307f244ee2
niwang55 <nickwang55@gmail.com>
2016-11-02 20:09:08 -0700

    Finish tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +11,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  var ret = false;
+
+  var iterator = function(currentTree) {
+    if (currentTree.value === target) {
+      ret = true;
+    }
+
+    if (currentTree.children.length > 0 ) {
+      for ( var i = 0; i < currentTree.children.length; i++) {
+        iterator(currentTree.children[i]);
+      }
+    }
+  };
+
+  iterator( this );
+  return ret;
 };
 
 
