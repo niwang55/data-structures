ed62b7fe19777b33ec7e85c64a70f87446e5e42c
niwang55 <nickwang55@gmail.com>
2016-11-04 14:30:15 -0700

    Implemented DoublyLinkedList and tests

---
+++ sprint-two/src/DoublyLinkedList.js
@@ -0,0 +1,75 @@
+var DoublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    if (list.head === null) {
+      var newNode = new Node(value);
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var nextNode = new Node(value);
+
+      list.tail.next = nextNode;
+      list.tail.next.previous = list.tail;
+      list.tail = list.tail.next;
+      
+    }
+  };
+
+  list.addToHead = function(value) {
+    if (list.head === null) {
+      var newNode = new Node(value);
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var nextNode = new Node(value);
+
+      list.head.previous = nextNode;
+      list.head.previous.next = list.head;
+      list.head = list.head.previous; 
+    }
+  };
+
+  list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
+  };
+
+  list.removeTail = function() {
+    var temp = list.tail;
+    list.tail = list.tail.previous;
+    return temp.value;
+  };
+
+  list.contains = function(target) {
+    var currentNode = list.head;
+
+    do {
+      if (currentNode.value === target) {
+        return true;
+      } 
+      currentNode = currentNode.next;
+    } while (currentNode !== null && currentNode.value !== undefined);
+
+    return false;
+  };
+
+  return list;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
