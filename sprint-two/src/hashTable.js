3fbb27c466ece5108a86ecea040bf5f045a2bbac
niwang55 <nickwang55@gmail.com>
2016-11-04 11:38:58 -0700

    Finish hashTable downsize

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,12 +3,12 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._counter = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var values = this._storage.get(index);
-
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, [[k, v]]);
   } else {
@@ -19,7 +19,23 @@ HashTable.prototype.insert = function(k, v) {
         this._storage.get(index).push([k, v]);
       }
     }
+  }
 
+  this._counter++;
+  
+  if (this._counter >= 0.75 * this._limit) {
+    this._limit *= 2;
+    this._counter = 0;
+    var temp = this._storage;
+    this._storage = LimitedArray(this._limit);
+
+    temp.each( function(bucket) {
+      if (bucket) {
+        for (var i = 0; i < bucket.length; i++) {
+          this.insert(bucket[i][0], bucket[i][1]);
+        }
+      }      
+    }.bind(this));
   }
 };
 
@@ -43,6 +59,23 @@ HashTable.prototype.remove = function(k) {
       c.splice( i, 1);
     }
   });
+
+  this._counter--;
+  
+  if (this._counter <= 0.25 * this._limit) {
+    this._limit /= 2;
+    this._counter = 0;
+    var temp = this._storage;
+    this._storage = LimitedArray(this._limit);
+
+    temp.each( function(bucket) {
+      if (bucket) {
+        for (var i = 0; i < bucket.length; i++) {
+          this.insert(bucket[i][0], bucket[i][1]);
+        }
+      }      
+    }.bind(this));
+  }
 };
 
 
